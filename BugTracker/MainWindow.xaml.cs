73e49783dc71a537742f450771d7449726241d21
Tyler Freedman <mail@tylerfreedman.com>
2013-02-01 18:37:57 -0500

    Users can now add projects to the DB

--- BugTracker/MainWindow.xaml.cs
+++ BugTracker/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace BugTracker {
     /// <summary>
@@ -20,6 +7,7 @@ namespace BugTracker {
     public partial class MainWindow : Window {
 
         string currentUser = "";
+        string currentProject = "";
 
         public MainWindow() {
             InitializeComponent();
@@ -27,6 +15,16 @@ namespace BugTracker {
 
         private void AddBug_Click(object sender, RoutedEventArgs e) {
             var dialog = new AddBug();
+            dialog.ShowDialog();
+        }
+
+        private void OpenProject_Click(object sender, RoutedEventArgs e) {
+            var dialog = new OpenProject();
+            dialog.ShowDialog();
+        }
+
+        private void NewProject_Click(object sender, RoutedEventArgs e) {
+            var dialog = new NewProject();
             dialog.ShowDialog();
         }
 
---
+++ BugTracker/NewProject.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Windows;
+
+namespace BugTracker {
+    /// <summary>
+    /// Interaction logic for NewProject.xaml
+    /// </summary>
+    public partial class NewProject : Window {
+        public NewProject() {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e) {
+            ArrayList list = new ArrayList();
+            StreamReader file = new StreamReader(@".\Projects.csv");
+            bool existingProjects = false;
+            string line;
+            while ((line = file.ReadLine()) != null) {
+                String projects = line;
+                list.Add(projects);
+            }
+            file.Close();
+            for (int i = 0; i < list.Count; i++) {
+                if (((string)(list[i])).Equals(ProjectName.Text)) {
+                    MessageBox.Show("There's already a project with that name. Please pick a different name for the new project.");
+                    existingProjects = true;
+                    break;
+                }
+            }
+            if (!existingProjects) {
+                StreamWriter sw = File.AppendText(@".\Projects.csv");
+                sw.WriteLine("");
+                sw.Write(ProjectName.Text);
+                sw.Flush();
+                sw.Close();
+                this.DialogResult = true;
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = false;
+        }
+    }
+}
